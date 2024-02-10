ad935352aad96576fa13c87922a76510f4ee0459
Prince Yadav <princeyadav00785@gmail.com>
2024-02-10 20:00:36 +0530

    create signin api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import  Jwt  from "jsonwebtoken";
 
 export const signup =  async (req,res,next)=>{
     const {username , email, password}=req.body;
@@ -25,3 +26,21 @@ export const signup =  async (req,res,next)=>{
 }
 
 };
+
+
+export const signin =  async (req,res,next)=>{
+    const {email, password}=req.body;
+   try {
+    const validUser = await User.findOne({email});
+    if(!validUser)return next(errorHandler(401,"User not found"));
+   const validPassowrd = bcryptjs.compareSync(password,validUser.password);
+   if(!validPassowrd)return next(errorHandler(401,"Invalid password"));
+   const token =Jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+   const {password:hashedPassword, ...rest}=validUser._doc;
+   const expiryDate = new Date(Date.now()+3600000);
+   res.cookie('access_token', token , {httpOnly:true, expires:expiryDate}).status(200).json(rest);
+
+   } catch (error) {
+    next(error);
+   }
+}
